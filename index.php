b5d1715b76fa7fcd4d4696c86e19a54ddbcf8c8a
santiagohewett <124379638+santiagohewett@users.noreply.github.com>
2023-04-14 11:42:12 +0000

    update

--- index.php
+++ index.php
@@ -3,31 +3,59 @@
   <head>
     <!-- Metadata -->
     <meta charset="utf-8">
-    <meta name="description" content="Fahrenheit to Celsius">
+    <meta name="description" content="Goals and assists to become the greatest soccer player">
     <meta name="keywords" content="immaculata, ics2o">
     <meta name="author" content="Santiago Hewett">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Link to CSS file -->
     <link rel="stylesheet" href="./css/style.css">
     <!-- Title -->
-    <title>Fahrenheit to Celsius</title>
+    <title>Goals and assists to become the greatest soccer player</title>
   </head>
   <body>
     <!-- php echo to print the html to the page -->
 <?php 
-echo "<h1>Fahrenheit to Celsius</h1>";
-echo "<h3>This program will calculate fahrenheit to celsius.</h3>";
+echo "<h1>Goals and Assists to Become the Greatest Soccer Player</h1>";
+echo "<h3>This program will calculate the goals needed to become the greatest goal scorer.</h3>";
 ?>
-<!-- form to get the measurements from the user -->
+<!-- form to get the goals per game from the user -->
     <form action="./results.php" method="post" target="results">
-      <label for=lblFahrenheit">Enter the fahrenheit:</label>
-      <input type="text" id="fahrenheit" placeholder="Enter the fahrenheit" name="fahrenheit"><br><br>
+      <label for=lblgoalsPerGame">Enter your goals per game:</label>
+      <input type="text" id="goalsPerGame" placeholder="Enter your goals per game" name="goalsPerGame"><br>
+    </form>
+
+    <!-- form to get the games played for goals from the user -->
+    <form action="./results.php" method="post" target="results">
+      <label for=lblgamesPlayedGoals">Enter your games played:</label>
+      <input type="text" id="gamesPlayedGoals" placeholder="Enter your games played" name="gamesPlayedGoals"><br><br>
       <input type="submit" value="Calculate">
     </form>
     
-    <!-- iframe for the results to show on the web page. -->
-    <iframe id="results" name="results">			
-      The degrees in celsius is " + $celsius + "°"
+    <!-- iframe for the results to show on the web page for goals -->
+    <iframe id="results1" name="results1">			
+      You need " + $goalsToBeTheBestAtGoals + " to become the best goal scorer of all time"
+    </iframe>
+
+    <!-- php echo to print the html to the page -->
+<?php 
+echo "<h3>This program will calculate the assists needed to become the greatest assister.</h3>";
+?>
+<!-- form to get the assists per game from the user -->
+    <form action="./results.php" method="post" target="results">
+      <label for=lblassistsPerGame">Enter your assists:</label>
+      <input type="text" id="assistsPerGame" placeholder="Enter your assists" name="assistsPerGame"><br>
+    </form>
+
+    <!-- form to get the games played for assists from the user -->
+    <form action="./results.php" method="post" target="results">
+      <label for=lblgamesPlayedAssists">Enter your games played:</label>
+      <input type="text" id="gamesPlayedAssists" placeholder="Enter your games played" name="gamesPlayedAssists"><br><br>
+      <input type="submit" value="Calculate">
+    </form>
+    
+    <!-- iframe for the results to show on the web page for assists -->
+    <iframe id="results2" name="results2">			
+      You need " + $assistsToBeTheBestAtAssists . " to become the best assister of all time"
     </iframe>
   </body>
 </html>
--- results.php
+++ results.php
@@ -1,11 +1,41 @@
 <?php
 
 // get the temperature from the textfields
-$fahrenheit = $_POST['fahrenheit'];
+$goalsPerGame = $_POST['goalsPerGame'];
+$gamesPlayedGoals = $_POST['gamesPlayedGoals'];
 
 // calculate the volume
-$celsius = (5/9) * ($fahrenheit - 32);
+$goals = $goalsPerGame * $gamesPlayedGoals;
+$goalsToBeTheBestAtGoals = $goals - 834;
+
+// if statments
+if ($goalsToBeTheBestAtGoals > "834") {
+  echo "Congratulations, you are already the best goal scorer!";
+} else {
+  echo "You need $goalsToBeTheBestAtGoals more goals to become the best goal scorer.";
+}
 
 // Display the volume
-echo "<h3>Results:</h3> The degrees in celsius is " . round($celsius, 2) . "°"
+echo "<h3>Results:</h3> You need " . $goalsToBeTheBestAtGoals . " to become the best goal scorer of all time"
+?>
+
+<?php
+
+// get the temperature from the textfields
+$assistsPerGame = $_POST['assistsPerGame'];
+$gamesPlayedAssists = $_POST['gamesPlayedAssists'];
+
+// calculate the volume
+$assists = $assistsPerGame * $gamesPlayedAssists;
+$assistsToBeTheBestAtAssists = $assists - 300;
+
+// if statments
+if ($assistsToBeTheBestAtAssists > "300") {
+  echo "Congratulations, you are already the best assister!";
+} else {
+  echo "You need $assistsToBeTheBestAtAssists more assists to become the best assister.";
+}
+
+// Display the volume
+echo "<h3>Results:</h3> You need " . $assistsToBeTheBestAtAssists . " to become the best assister of all time"
 ?>
