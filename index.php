0a24e5d88a26f1faa38933699fa66f6a5aaf513d
santiagohewett <124379638+santiagohewett@users.noreply.github.com>
2023-04-18 08:52:06 +0000

    Update

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <!-- php echo to print the html to the page -->
 <?php 
 echo "<h1>Goals and Assists to Become the Greatest Soccer Player</h1>";
-echo "<h3>This program will calculate the goals needed to become the greatest goal scorer.</h3>";
+echo "<h3>This program will calculate the goals and assists needed to become the greatest soccer player of all time.</h3>";
 ?>
 <!-- form to get the goals per game from the user -->
     <form action="./results.php" method="post" target="results">
@@ -26,37 +26,20 @@ echo "<h3>This program will calculate the goals needed to become the greatest go
 
     <!-- form to get the games played for goals from the user -->
     <form action="./results.php" method="post" target="results">
-      <label for=lblgamesPlayedGoals">Enter your games played:</label>
-      <input type="text" id="gamesPlayedGoals" placeholder="Enter your games played" name="gamesPlayedGoals"><br><br>
-      <input type="submit" name="button1" class="button" value="Calculate" />
-    
-      
+      <label for=lblgamesPlayedGoalsAndAssists">Enter your games played:</label>
+      <input type="text" id="gamesPlayedGoalsAndAssists" placeholder="Enter your games played" step="1" min="1" name="gamesPlayedGoalsAndAssists"><br>
     </form>
-    
-    <!-- iframe for the results to show on the web page for goals -->
-    <iframe id="results1" name="results1">			
-      You need " + $goalsToBeTheBestAtGoals + " to become the best goal scorer of all time"
-    </iframe>
 
-    <!-- php echo to print the html to the page -->
-<?php 
-echo "<h3>This program will calculate the assists needed to become the greatest assister.</h3>";
-?>
-<!-- form to get the assists per game from the user -->
+    <!-- form to get the assists per game from the user -->
     <form action="./results.php" method="post" target="results">
-      <label for=lblassistsPerGame">Enter your assists:</label>
+      <label for=lblassistsPerGame">Enter your assists per game:</label>
       <input type="text" id="assistsPerGame" placeholder="Enter your assists" name="assistsPerGame"><br>
-    </form>
-
-    <!-- form to get the games played for assists from the user -->
-    <form action="./results.php" method="post" target="results">
-      <label for=lblgamesPlayedAssists">Enter your games played:</label>
-      <input type="text" id="gamesPlayedAssists" placeholder="Enter your games played" name="gamesPlayedAssists"><br><br>
-      <input type="submit" name="button1" class="button" value="Calculate" />
+      <input type="submit" name="button" class="button" value="Calculate" />
     </form>
     
-    <!-- iframe for the results to show on the web page for assists -->
-    <iframe id="results2" name="results2">			
+    <!-- iframe for the results to show on the web page for goals and assists -->
+    <iframe id="results" name="results">			
+      You need " + $goalsToBeTheBestAtGoals + " to become the best goal scorer of all time"<br>
       You need " + $assistsToBeTheBestAtAssists . " to become the best assister of all time"
     </iframe>
   </body>
--- results.php
+++ results.php
@@ -1,22 +1,18 @@
 <?php
 
-  if(array_key_exists('button1', $_POST)) {
-            calculateGoals();
-        }
-        else if(array_key_exists('button2', $_POST)) {
-            calculateAssists();
-        }
-
-function calculateGoals() {  
-  // get the temperature from the textfields
+function calculateGoalsAndAssists() {  
+  // get the goals, assists and game playes from the textfields
   $goalsPerGame = $_POST['goalsPerGame'];
-  $gamesPlayedGoals = $_POST['gamesPlayedGoals'];
-  
-  // calculate the volume
-  $goals = $goalsPerGame * $gamesPlayedGoals;
+  $gamesPlayedGoalsAndAssists = $_POST['gamesPlayedGoalsAndAssists'];
+  $assistsPerGame = $_POST['assistsPerGame'];
+
+  // calculate goals and assists
+  $goals = $goalsPerGame * $gamesPlayedGoalsAndAssists;
   $goalsToBeTheBestAtGoals = $goals - 834;
+  $assists = $assistsPerGame * $gamesPlayedGoalsAndAssists;
+  $assistsToBeTheBestAtAssists = $assists - 300;
   
-  // if statments
+  // if statments for goals
   if ($goalsToBeTheBestAtGoals > "834") {
     echo "Congratulations, you are already the best goal scorer!";
   }
@@ -25,22 +21,11 @@ function calculateGoals() {
     $goalsToBeTheBestAtGoals * -1;
     echo "<h3>Results:</h3> You need " . $goalsToBeTheBestAtGoals . " to become the best goal scorer of all time";
   }
-  
-  // Display the volume
+  else {
   echo "<h3>Results:</h3> You need " . $goalsToBeTheBestAtGoals . " to become the best goal scorer of all time";
 }
 
-function calculateAssists() {  
-
-  // get the temperature from the textfields
-  $assistsPerGame = $_POST['assistsPerGame'];
-  $gamesPlayedAssists = $_POST['gamesPlayedAssists'];
-  
-  // calculate the volume
-  $assists = $assistsPerGame * $gamesPlayedAssists;
-  $assistsToBeTheBestAtAssists = $assists - 300;
-  
-  // if statments
+  // if statments for assists
   if ($assistsToBeTheBestAtAssists > "300") {
     echo "Congratulations, you are already the best assister!";
   }
@@ -49,8 +34,8 @@ function calculateAssists() {
     $assistsToBeTheBestAtAssists * -1;
     echo "<h3>Results:</h3> You need " . $assistsToBeTheBestAtAssists . " to become the best assister of all time";
   }
-  
-  // Display the volume
+  else {
   echo "<h3>Results:</h3> You need " . $assistsToBeTheBestAtAssists . " to become the best assister of all time";
+}
 }
 ?>
